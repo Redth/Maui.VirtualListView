aa57ff52b4eb22fdbdf623b8ad742233111c247b
Jonathan Dick <jondick@gmail.com>
2021-09-01 14:04:38 -0400

    Use MauiAppBuilder for extension method

--- VirtualListView/HostBuilderExtensions.cs
+++ VirtualListView/HostBuilderExtensions.cs
@@ -10,8 +10,8 @@ namespace Microsoft.Maui
 {
     public static class VirtualListViewHostBuilderExtensions
     {
-        public static IAppHostBuilder UseVirtualListView(this IAppHostBuilder appHostBuilder)
-            => appHostBuilder.ConfigureMauiHandlers(handlers =>
+        public static MauiAppBuilder UseVirtualListView(this MauiAppBuilder builder)
+            => builder.ConfigureMauiHandlers(handlers =>
                 handlers.AddHandler(typeof(IVirtualListView), typeof(VirtualListViewHandler)));
     }
 
