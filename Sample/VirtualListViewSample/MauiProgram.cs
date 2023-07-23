774b95e406a1eb9bd64e30b206de90c47310ae8d
redth <jondick@gmail.com>
2023-07-23 11:20:38 -0400

    Lazy load music database instance singleton

--- Sample/VirtualListViewSample/MauiProgram.cs
+++ Sample/VirtualListViewSample/MauiProgram.cs
@@ -1,19 +1,16 @@
-using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Controls.Hosting;
-using Microsoft.Maui.LifecycleEvents;
+namespace VirtualListViewSample;
 
-namespace VirtualListViewSample
+public class MauiProgram
 {
-	public class MauiProgram
+	public static MauiApp Create()
 	{
-		public static MauiApp Create()
-		{
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				.UseVirtualListView();
-			return builder.Build();
-		}
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseVirtualListView();
+
+		_ = Task.Run(() => MusicDatabase.Instance.Value);
+
+		return builder.Build();
 	}
 }
--- Sample/VirtualListViewSample/MusicDataAdapter.cs
+++ Sample/VirtualListViewSample/MusicDataAdapter.cs
@@ -1,86 +1,87 @@
 ﻿using LiteDB;
 using Microsoft.Maui.Adapters;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 
-namespace VirtualListViewSample
+namespace VirtualListViewSample;
+
+public static class MusicDatabase
 {
-	public static class MusicDatabase
+	public static Lazy<LiteDatabase> Instance { get; }
+		= new Lazy<LiteDatabase>(() => Open());
+
+	static LiteDatabase Open()
 	{
-		public static LiteDatabase Open()
-		{
 		var files = new[] { "chinook.litedb" };
 
-			foreach (var file in files)
-			{
-				var path = Path.Combine(FileSystem.CacheDirectory, file);
+		foreach (var file in files)
+		{
+			var path = Path.Combine(FileSystem.CacheDirectory, file);
 
-				var txt = string.Empty;
+			var txt = string.Empty;
 
-				using (var stream = typeof(App).Assembly.GetManifestResourceStream("VirtualListViewSample." + file))
-				using (var sw = File.Create(path))
-					stream.CopyTo(sw);
-			}
-
-			var dbPath = Path.Combine(FileSystem.CacheDirectory, files.First());
-
-			return new LiteDatabase(dbPath);
+			using (var stream = typeof(App).Assembly.GetManifestResourceStream("VirtualListViewSample." + file))
+			using (var sw = File.Create(path))
+				stream.CopyTo(sw);
 		}
+
+		var dbPath = Path.Combine(FileSystem.CacheDirectory, files.First());
+
+		return new LiteDatabase(dbPath);
 	}
-
-	public class MusicDataAdapter : VirtualListViewAdapterBase<AlbumInfo, TrackInfo>
-	{
-		public MusicDataAdapter()
-		{
-			database ??= MusicDatabase.Open();
-
-			albums = database.GetCollection<AlbumInfo>("albums").FindAll().OrderBy(a => a.AlbumId).ToList();
-			tracks = database.GetCollection<TrackInfo>("tracks");
-		}
-
-		LiteDatabase database;
-		readonly List<AlbumInfo> albums;
-		readonly ILiteCollection<TrackInfo> tracks;
-		
-		public override int Sections
-			=> albums.Count();
-
-		public override TrackInfo Item(int sectionIndex, int itemIndex)
-		{
-			var section = Section(sectionIndex) as AlbumInfo;
-
-			var t = tracks.Query().Where(t => t.AlbumId == section.AlbumId).OrderBy(t => t.TrackId).Skip(itemIndex).Limit(1).First();
-
-			t.SectionIndex = sectionIndex;
-			t.ItemIndex = itemIndex;
-
-			return t;
-		}
-
-		public override int ItemsForSection(int sectionIndex)
-			=> (Section(sectionIndex) as AlbumInfo).TrackCount;
-
-		public override AlbumInfo Section(int sectionIndex)
-		{
-			var section = albums[sectionIndex] as AlbumInfo;
-			if (section.TrackCount <= 0)
-				section.TrackCount = tracks.Count(t => t.AlbumId == section.AlbumId);
-
-			return section;
-		}
-	}
-
-
-	public class AlbumSection : ObservableCollection<TrackInfo>
-	{
-		public AlbumSection(AlbumInfo album, IEnumerable<TrackInfo> tracks)
-			: base(tracks)
-		{
-			Album = album;
-		}
-
-		public AlbumInfo Album { get; private set; }
-	}
-
-	
 }
+
+public class MusicDataAdapter : VirtualListViewAdapterBase<AlbumInfo, TrackInfo>
+{
+	public MusicDataAdapter()
+	{
+		database ??= MusicDatabase.Instance.Value;
+
+		albums = database.GetCollection<AlbumInfo>("albums").FindAll().OrderBy(a => a.AlbumId).ToList();
+		tracks = database.GetCollection<TrackInfo>("tracks");
+	}
+
+	LiteDatabase database;
+	readonly List<AlbumInfo> albums;
+	readonly ILiteCollection<TrackInfo> tracks;
+	
+	public override int Sections
+		=> albums.Count();
+
+	public override TrackInfo Item(int sectionIndex, int itemIndex)
+	{
+		var section = Section(sectionIndex) as AlbumInfo;
+
+		var t = tracks.Query().Where(t => t.AlbumId == section.AlbumId).OrderBy(t => t.TrackId).Skip(itemIndex).Limit(1).First();
+
+		t.SectionIndex = sectionIndex;
+		t.ItemIndex = itemIndex;
+
+		return t;
+	}
+
+	public override int ItemsForSection(int sectionIndex)
+		=> (Section(sectionIndex) as AlbumInfo).TrackCount;
+
+	public override AlbumInfo Section(int sectionIndex)
+	{
+		var section = albums[sectionIndex] as AlbumInfo;
+		if (section.TrackCount <= 0)
+			section.TrackCount = tracks.Count(t => t.AlbumId == section.AlbumId);
+
+		return section;
+	}
+}
+
+
+public class AlbumSection : ObservableCollection<TrackInfo>
+{
+	public AlbumSection(AlbumInfo album, IEnumerable<TrackInfo> tracks)
+		: base(tracks)
+	{
+		Album = album;
+	}
+
+	public AlbumInfo Album { get; private set; }
+}
+
+
